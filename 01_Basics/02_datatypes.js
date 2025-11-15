91852cf975700c049f4996f19798f06983578684
Vaibhav Patil <vaibhav.patil.dev9@gmail.com>
2025-11-15 04:44:50 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";
+let num = 97456789876786787345678888888887654
+let name = "Vaibahv"
+let isLogin = false
+let temp = null
+let address;
+
+console.table([num, name , isLogin, temp,address]);
+console.log(typeof isLogin);
+
+
