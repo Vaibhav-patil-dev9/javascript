39f5fddb4de1452ee095e40ecb6ec9e8ce60a84e
Vaibhav Patil <vaibhav.patil.dev9@gmail.com>
2025-11-15 04:45:26 +0000

    variables in js

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,8 @@
+const accountId = 123321
+let accountName = "Vaibhav"
+var accounttype = "saving"
+accountCity = "Pune"
+let address;
+
+// console.log(accountId,accountName,accounttype,accountCity);
+console.table([accountId,accountName,accounttype,accountCity,address])
