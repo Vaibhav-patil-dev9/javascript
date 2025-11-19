17b3c1ff3f218b0507875cb8a3edc2bee0622843
Vaibhav Patil <vaibhav.patil.dev9@gmail.com>
2025-11-19 22:37:04 +0000

    Comparison Operations

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,15 @@
+console.log("2" > 1);
+console.log("02" == 2); 
+console.log("02" === 2);
+// never do above conversions in real code
+console.log(null >= 0);
+console.log(undefined <= 0);
+console.log(null == 0);
+console.log(undefined == 0);
+console.log(NaN == NaN);
+console.log(NaN === NaN);
+console.log(NaN != NaN);
+console.log(NaN !== NaN);  
+
+
+// === use this for comparison it is check datatypes also..
