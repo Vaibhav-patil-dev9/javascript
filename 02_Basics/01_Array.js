3e31780303ee373f6fe74e77e195eaec8390198d
Vaibhav Patil <vaibhav.patil.dev9@gmail.com>
2025-11-22 02:24:40 +0000

    array basics

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,36 @@
+const myArr = [0,1,2,3,4,5];
+// console.log(myArr[3]);
+// console.log(myArr);
+
+
+myArr.push(7);
+// console.log(myArr);
+myArr.pop();
+
+myArr.unshift(9);
+myArr.shift();
+// console.log(myArr.includes(5));
+
+// console.log(myArr.indexOf(7));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof (newArr));
+
+// imp question difference between slice and splice
+
+console.log("A " , myArr)
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1)
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log(myn2)
+console.log("c ", myArr);
+
+
+
+
+  
