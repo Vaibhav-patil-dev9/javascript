3edbea38c10ccbfd6f70a9a68e4e0986f7ca4723
Vaibhav Patil <vaibhav.patil.dev9@gmail.com>
2025-11-25 08:54:53 +0000

    json talk and Object de-structure

---
+++ 02_Basics/05_object.js
@@ -0,0 +1,11 @@
+
+
+const course = {
+    courseName : "javacript in hindi",
+    courseFees:0,
+    courseInstructor:"Hitesh Sir"
+}
+// Object de-Structure
+const {courseInstructor:instructor} = course
+console.log(instructor);
+
